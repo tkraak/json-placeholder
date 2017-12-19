fb55480187a730a471bcf87b4077c51a25549cca
Tom Kraak <thomas.kraak@teamaol.com>
2017-12-19 15:01:02 -0500

    model --> posts

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -3,5 +3,8 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.store.findAll('post');
+  },
+  setupController(controller, model) {
+    controller.set('posts', model)
   }
 });
